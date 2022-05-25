c8948714025cb1ca8b567899ffd6055efeb97925
junseokoh-hub <lunulatecat@naver.com>
2022-05-25 16:30:02 +0900

    using useMemo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState, useRef, useEffect} from "react";
+import {useState, useRef, useEffect, useMemo} from "react";
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
@@ -75,10 +75,22 @@ function App() {
     setData(
       data.map((it) => it.id === targetId ? {...it, content:newContent} : it)
     )
-  }
+  };
+  const getDiaryAnalysis = useMemo(() => {
+    console.log('일기 분석 시작');
+    const goodCount = data.filter((it)=>it.emotion >= 3).length;
+    const badCount = data.length-goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return {goodCount, badCount, goodRatio}; 
+  },[data.length]);
+  const {goodCount, badCount, goodRatio} = getDiaryAnalysis;
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate}/>
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data}/>
     </div>
   );
